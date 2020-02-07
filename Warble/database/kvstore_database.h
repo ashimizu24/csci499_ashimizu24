86876e650e284eb3dc1b8bec79795953e3354707
ashimizu24 <ashimizu@usc.edu>
2020-02-06 23:54:09 -0800

    started creating database and started unit tests for it

---
+++ Warble/database/kvstore_database.h
@@ -0,0 +1,84 @@
+#include <string>
+#include <unordered_map>
+#include <iostream>
+#include <vector>
+
+// Macros for prefixes 
+#define USR_PRE "USR"
+#define WARB_PRE "WAR"
+
+// Macros for warble id
+#define WARBLE_CNT 0
+#define USER_CNT 0
+
+struct User {
+  User(std::string username, int id){
+    this->username = username;
+    this->id = id;
+  }
+
+  std::string username;
+  int id; //Do we need
+  std::vector<std::string> followers; //or int for ids
+
+  std::string toString()
+  {
+    std::string followers;
+    for(const auto& user : followers){
+      followers = followers + user + ",";
+    }
+    return username + ";" + std::to_string(id) + ";" + followers;
+  }
+};
+
+struct Reply {
+  Reply(int reply_id, int user_id, std::string message){
+    this->reply_id = reply_id;
+    this->user_id = user_id;
+    this->message = message;
+  }
+
+  int reply_id;
+  int user_id;
+  std::string message;
+
+  std::string toString()
+  {
+    return message + ";" + std::to_string(reply_id);
+  }
+};
+
+struct Warble {
+  Warble(int id, std::string message, std::string username){
+    this->id = id;
+    this->message = message;
+    this->username = username;
+  }
+
+  int id;
+  std::string message;
+  std::string username;
+  std::vector<Reply> replies;
+
+  std::string toString()
+  {
+    return message + ";" + std::to_string(id);
+  }
+};
+
+class UserMap {
+public:
+  std::unordered_map<std::string,int> GetDb();
+
+  User GetUser(std::string username);
+  void AddUser(std::string username);
+  int GetUserId(std::string username);
+
+  Warble GetWarble(int id);
+  void AddWarble(std::string message, std::string username);
+
+
+private:
+  std::unordered_map<std::string, std::string> db; //database mapping key (prefix+id/username) to serialized struct
+
+};
---
+++ Warble/database/kvstore_database_test.cc
@@ -0,0 +1,48 @@
+#include "kvstore_database.h"
+
+#include "gtest/gtest.h"
+
+//to compile: g++ kvstore_database_test.cc -o dbtest -lgflags -pthread -lprotobuf -lpthread
+
+// The fixture for testing class Foo.
+class DBTest : public ::testing::Test {
+ protected:
+  // You can remove any or all of the following functions if their bodies would
+  // be empty.
+
+  DBTest() {
+     // You can do set-up work for each test here.
+  }
+
+  ~DBTest() override {
+     // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  void SetUp() override {
+     // Code here will be called immediately after the constructor (right
+     // before each test).
+  }
+
+  void TearDown() override {
+     // Code here will be called immediately after each test (right
+     // before the destructor).
+  }
+
+  // Class members declared here can be used by all tests in the test suite
+  // for Foo.
+};
+
+TEST(Database, User) {
+	UserMap* map;
+	map->AddUser("ayashimizu");
+	EXPECT_EQ(map->GetUserId("ayashimizu"), 0);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  std::cout << "RUNNING DB TESTS\n";
+  return RUN_ALL_TESTS();
+}
